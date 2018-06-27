8fa119e59387db6142c1fed9702aa4d592a65305
Daniel <daniel@daniels-mbp-2.gateway.sonic.net>
2018-06-27 16:01:21 -0700

    created new django app + put stable-req based on current pip, also logged initial readme

---
+++ README.md
@@ -0,0 +1,13 @@
+**Development
+
+***Started with a fresh macbook pro without pip***
+6/27 - Install homebrew, install pip
+6/27 - Creat initial django backend project + git repo
+6/27 - Set up django app with postgres backend
+6/27 - Isolated to sub director for seperation from React App
+6/27 - Created first commit
+6/27 - Set up pip, saved hard version in stable-req.txt
+6/27 - Ran migrate + run server to see first 'django powerd web page'
+
+
+
--- djangoBackWeEat/djangoBackWeEat/settings.py
+++ djangoBackWeEat/djangoBackWeEat/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'djangoBackWeEat.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_env_variable('DATABASE_NAME'),
-        'USER': get_env_variable('DATABASE_USER'),
-        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'NAME': 'we_eat', #get_env_variable('DATABASE_NAME'),
+        'USER': 'daniel', #get_env_variable('DATABASE_USER'),
+        'PASSWORD': '', #get_env_variable('DATABASE_PASSWORD'),
         'HOST': '',
         'PORT': '',
     }
