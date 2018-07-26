5de722b3d002ad079756775c345fba812d5f49e6
daniel.anderson <daniel.anderson@weadmins-MacBook-Pro-2.local>
2018-07-26 14:25:37 +0300

    adding storybook, and updated models

--- we_eat_app/models.py
+++ we_eat_app/models.py
@@ -3,11 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Resteraunts(models.Model):
-    # song title
     name = models.CharField(max_length=255, null=False)
-    # name of artist or group/band
-    rating = models.CharField(max_length=255, null=False) #make this a python enum
+    rating = models.FloatField(null=False)
+    lat = models.FloatField(null=True)
+    lng = models.FloatField(null=True)
+    genre_string = models.CharField(max_length=255, null=True)
+    hours = models.CharField(max_length=255, null=True)
+    primary_image_url = models.CharField(max_length=255, null=True)
+    company_site_url = models.CharField(max_length=255, null=True)
 
     def __str__(self):
         return "{} - {}".format(self.name, self.rating)
-
